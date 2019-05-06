94fb6cc10dec33e892ab0f21daa014ee68c0821d
kpan7202 <30652259+kpan7202@users.noreply.github.com>
2019-05-06 19:34:25 +0700

    Add files via upload

---
+++ week6/homework_6_5/mongo-create replica set.bat
@@ -0,0 +1,6 @@
+"C:\Program Files\MongoDB 2.6 Standard\bin\mongod" --replSet m101 --logpath "1.log" --dbpath D:\Mongo\data\rs1 --port 27017 --smallfiles --oplogSize 64
+ 
+"C:\Program Files\MongoDB 2.6 Standard\bin\mongod" --replSet m101 --logpath "2.log" --dbpath D:\Mongo\data\rs2 --port 27018 --smallfiles --oplogSize 64
+
+"C:\Program Files\MongoDB 2.6 Standard\bin\mongod" --replSet m101 --logpath "3.log" --dbpath D:\Mongo\data\rs3 --port 27019 --smallfiles --oplogSize 64
+
